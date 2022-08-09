52b425dc1df28559c22f15166ca4c50a6e112fec
chandanasambangi <chandanasambangi.1@gmail.com>
2022-08-09 13:12:58 +0530

    Mean

--- src/MeanMedianMode.java
+++ src/MeanMedianMode.java
@@ -7,7 +7,13 @@ public class MeanMedianMode {
         for(int i=0;i<size;i++){
             array[i]=sc.nextInt();
         }
+        //mean
         System.out.println("mean is : " + mean(array));
+        //median
+        Arrays.sort(array);
+        System.out.println("median is : " + median(array));
+
+
 
     }
     public static double mean(int[] array){
@@ -17,6 +23,17 @@ public class MeanMedianMode {
             sum=sum+array[i];
         }
         result=sum/(array.length);
+        return result;
+    }
+    public static double median(int[] array){
+        double result=0.0;
+        int size=array.length;
+        if(size%2 == 0){
+            result= (double)((array[size/2 - 1] + array[size/2]))/28;
+        }
+        else{
+            result= (double)(array[(int)size/2]);
+        }
         return result;
     }
 }
