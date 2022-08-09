2cfaabd750db5013c116c5156e21765c385a646f
chandanasambangi <chandanasambangi.1@gmail.com>
2022-08-09 21:15:40 +0530

    Ranks of each student is calculated

--- src/Student.java
+++ src/Student.java
@@ -3,7 +3,7 @@ public class Student {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int numberOfStudents=15;
-        int marks[][] = new int[15][7];
+        int marks[][] = new int[15][6];
         int total=0;
         for(int i=0;i<15;i++){
             System.out.println("Enter Student"+(i+1)+" marks");
@@ -15,6 +15,9 @@ public class Student {
             }
             marks[i][5]=studentTotal;
         }
+        total(marks);
+        rank(marks);
+
 
     }
     public static void total(int[][] marks){
@@ -22,4 +25,24 @@ public class Student {
             System.out.println("Total of Student"+(i+1)+" is "+marks[i][5]);
         }
     }
+    public static void rank(int[][] marks){
+        int[][] rankArray=new int[15][2];
+        for(int i=0;i<15;i++){
+            rankArray[i][0]=i;
+            rankArray[i][1]=marks[i][5];
+        }
+        Arrays.sort(rankArray, new Comparator<int[]>() {
+            public int compare(final int[] entry1, final int[] entry2) {
+                if (entry1[1] > entry2[1])
+                    return 1;
+                else
+                    return -1;
+            }
+        });
+        for(int i=0;i<15;i++){
+            System.out.println( "rank "+(i+1)+" - Student"+(rankArray[i][0] +1 ));
+        }
+
+    }
+
 }
